6f65065fe6e8ce0ce6ac1e567d2303106920eb25
JeshuaBenitez <tucorreo@ejemplo.com>
2025-07-26 23:51:07 -0600

    fix 2: mejora visual el móvil, evita parpadeo blanco

--- src/App.js
+++ src/App.js
@@ -6,17 +6,16 @@ import ContactSection from "./components/ContactSection";
 import Footer from "./components/Footer";
 import InfoSeccion from "./components/InfoSecction";
 
-
 function App() {
   return (
-    <>
+    <div className="min-h-screen w-full overflow-x-hidden bg-gradient-to-r from-purple-900 to-purple-500">
       <HeroSection />
       <InfoSeccion />
       <InfoJovenes />
       <InfoAdolescentes />
       <ContactSection />
       <Footer />
-    </>
+    </div>
   );
 }
 
